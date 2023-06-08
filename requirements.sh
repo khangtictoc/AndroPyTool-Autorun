527e5faa2f7d0e2b56a102b122a41b7bcb8b9da6
Tran Hoang Khang <48288606+khangtictoc@users.noreply.github.com>
2023-06-08 14:59:43 +0700

    Create requirements.sh

---
+++ requirements.sh
@@ -0,0 +1,9 @@
+#! /bin/bash
+
+# Pre-run: Installing required packages (root privilege)
+
+apt install -y curl 
+apt install -y net-tools
+apt install -y bc 
+apt install -y jq
+apt install -y docker.io
